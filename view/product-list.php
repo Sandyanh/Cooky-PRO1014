1d2905f29f8f5fadbd1ccf405a26904cabdadefe
Phuong Anh <phamphuonganh5112002@gmail.com>
2024-03-19 19:30:57 +0700

    style: Tạo giao diện trang danh sách sản phẩm

---
+++ view/product-list.php
@@ -0,0 +1,33 @@
+<!-- Danh sách sản phẩm theo danh mục -->
+<main class="page-container">
+    <div class="page-wrapper">
+        <div class="home-page-container">
+            <!-- Category List -->
+            <div class="short-link-list">
+                <div class="swiper-container swiper-container-pointer-events">
+                    <div class="swiper-wrapper">
+                        <div class="category-slider category-list-product-page">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Product List -->
+            <div class="group-product-content">
+                <div class="title">✨ Thực đơn - Thịt Bò ✨&nbsp;<small class="total-product"><b></b> sản phẩm</small></div>
+                <div class="content-product-container">
+                    <div class="promotion-box">
+                        
+                    </div>
+                </div>
+            </div>
+            <!-- Product List by view -->
+            <div class="group-product-content">
+                <div class="title">❤️️ Món ăn yêu thích ❤️️</div>
+                <div class="content-product-container">
+                    <div class="promotion-box">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
